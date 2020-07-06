2a8cc88cf427adb56a6693d1b9111f235a7d6618
mcanitez <mehmetcanitez@yandex.com>
2020-07-07 00:26:37 +0300

    Add project files.

---
+++ instagram_authorization/Controllers/InstagramLLTController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace instagram_authorization.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InstagramLLTController : ControllerBase//instagram long-lived token
+    {
+        private readonly IConfiguration _configuration;
+
+        public InstagramLLTController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string InstagramLLT(string shortLivedToken)
+        {
+            var str= _configuration["InstagramApiKey:Key"];
+            return  _configuration["InstagramApiKey:Key"];
+
+        }
+    }
+}
